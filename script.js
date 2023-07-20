37c633d17dd4abb310268e974e6ceb79595ee213
Allen Thomas <82997175+allenthomas01@users.noreply.github.com>
2023-07-20 12:40:28 +0530

    add files

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['\nOpen Source Advocate', '\nSoftware Developer',' CSE Student'],
+    typeSpeed: 70,
+    loop:true,
+    backDelay: 900,
+    backSpeed: 30,
+  });
+
+  
